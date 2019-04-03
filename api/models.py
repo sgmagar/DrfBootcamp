b2980f28416ee48ad93e4bcd6d2c248016689436
Saurav <sp.gharti@gmail.com>
2019-04-03 18:30:49 +0545

    create product model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Creation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Product(Creation):
+    name = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
