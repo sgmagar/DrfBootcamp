fcd293e2dd843eeb38c4a7ce6ece3cc0e5a8febb
Saurav <sp.gharti@gmail.com>
2019-04-04 17:24:36 +0545

    add login url

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import HelloAPIView, HelloViewset, ProductViewset, UserViewset
+from api.views import (
+    HelloAPIView, HelloViewset, ProductViewset, UserViewset, LoginViewset
+)
 
 router = DefaultRouter()
 router.register("hello-viewset", HelloViewset, base_name="hello_viewset")
 router.register("products", ProductViewset)
 router.register("users", UserViewset)
+router.register("login", LoginViewset, base_name="login")
 
 
 urlpatterns = [
