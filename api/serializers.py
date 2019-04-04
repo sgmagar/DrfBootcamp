40e8a13515def97cb0e4bd7be37e2082d1984d59
Saurav <sp.gharti@gmail.com>
2019-04-04 16:52:35 +0545

    add user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from api.models import Product
@@ -9,3 +10,24 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ["name", "price", "created_at", "updated_at"]
         read_only_fields = ["created_at", "updated_at"]
+
+
+class UserSerliazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        user = User(
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+            username=validated_data["email"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
