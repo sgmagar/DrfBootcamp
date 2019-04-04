3eafe85dc0144b27d63b173e39785b7ca8ddb2af
Saurav <sp.gharti@gmail.com>
2019-04-04 19:18:06 +0545

    mf

--- testapi/settings.py
+++ testapi/settings.py
@@ -87,19 +87,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'testapi.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# # Database
+# # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-if not DEBUG:
-    DATABASES = {
-        "default": env.db()
-    }
+DATABASES = {
+    "default": env.db()
+}
 
 
 
